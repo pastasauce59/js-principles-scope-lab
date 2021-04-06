336a6d985de222c55d19cebb222eb83259c94640
Michael Primavera <pastasauce59@gmail.com>
2021-04-06 09:48:46 -0400

    commit tree 0256ddf5d7afb25831c24353b4c8e0c5a5f24396

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob"
+
+const leastFav = "Jim"
+
+function upperCaseCustomerName(){
+    customerName = "BOB"
+}
+    
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFav = "Jon";
+}
